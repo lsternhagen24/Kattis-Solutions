42d7dc3cfaee571845fa001a3bdd3370fa6ec122
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-19 19:32:48 -0600

    Update ADifferentProblem.java

--- ADifferentProblem.java
+++ ADifferentProblem.java
@@ -2,24 +2,25 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author lucas
+ Solution to kattis problem A Different Problem
+ https://open.kattis.com/problems/different
  */
 public class ADifferentProblem {
     public static void main(String[] args)throws Exception {
         Scanner sc = new Scanner(System.in);
           String line = "";
+        //read while not end of file
         while (sc.hasNextLine()) {
             line = sc.nextLine();
+            //use biginteger class since integers can be 15^15
             BigInteger a = new BigInteger(line.substring(0,line.indexOf(" ")));
             BigInteger b = new BigInteger(line.substring(line.indexOf(" ")+1));
+            //call subtract to get difference
             BigInteger c = a.subtract(b);
+            //print output
             System.out.println(c.abs());
         }
     }
