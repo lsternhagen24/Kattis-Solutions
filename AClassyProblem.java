624416fe1a6d951fe831af6ae999f5cdf1c8809f
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-19 19:30:18 -0600

    Update AClassyProblem.java

--- AClassyProblem.java
+++ AClassyProblem.java
@@ -3,14 +3,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author lucas
+ Solution to kattis problem A Classy Problem
+ https://open.kattis.com/problems/classy
  */
 public class AClassyProblem {
 
@@ -29,27 +26,37 @@ public class AClassyProblem {
         }
 
     }
-
+//class Person, each person has a rank and name
     public static class Person implements Comparable<Person> {
+        //array of integers that holds rank. Each rand can be 10 long. Upper=3, Middle=2, Lower=1
         int[] rank = new int[10];
+        //name for tie-breakers
         String name;
-        
+        //constructor for creating a person. 
         public Person(String[] line){
+            //sets the name from line
             this.name = line[0];
+            //reads the rank from input line
             String[] r = line[1].trim().split(" ")[0].split("-");
             int i = 0;
+            //initializes the rank array from input
             while(i<r.length){
+                //read in reverse since rank is right to left
                 int rI = r.length-1-i;
                 rank[i++] = r[rI].equals("upper") ? (3) : (r[rI].equals("middle") ? 2 : 1);
             }
+            //all classes not inilialized are middle
             while(i < 10) rank[i++] = 2;
         }
-
+        //compare two Persons. 
         @Override
         public int compareTo(Person t) {
             int i = 0;
+            //find first difference in rank array
             while(i < 9 && rank[i] == t.rank[i]) i++;
+            //if they are the same rank return alphabetical comparison
             if(rank[i] == t.rank[i]) return -name.compareTo(t.name);
+            //else return the integer difference
             return rank[i] - t.rank[i];
         }
     }
