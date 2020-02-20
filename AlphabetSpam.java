b8ed97ae5ac323ac33589d3f409d4d16209a0b27
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-19 19:59:01 -0600

    Update AlphabetSpam.java

--- AlphabetSpam.java
+++ AlphabetSpam.java
@@ -3,9 +3,12 @@ import java.util.*;
 
 /**
  * @author L-Sternhagen
+ Kattis solution to Alphabet Spam
+ https://open.kattis.com/problems/alphabetspam
  */
 public class AlphabetSpam {
     public static void main(String[] args) {
+        //read input string to character array
         Scanner sc = new Scanner(System.in);
         char[] input = sc.nextLine().toCharArray();
         int white = 0;
@@ -14,6 +17,7 @@ public class AlphabetSpam {
         int upper = 0;
         int special = 0;
         
+        //loop through every character and classify it as white, lower, upper, or special
         for(int i = 0; i < input.length; i++){
             int n = input[i];
             if(n >= 97 && n <= 122)lower++;
@@ -24,11 +28,12 @@ public class AlphabetSpam {
             total++;
         
         }
+        //calculate ratios
         double a = ((double)white)/((double)total);
         double b = ((double)lower)/((double)total);
         double c = ((double)upper)/((double)total);
         double d = ((double)special)/((double)total);
+        //print output
         System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
-        
     }
 }
