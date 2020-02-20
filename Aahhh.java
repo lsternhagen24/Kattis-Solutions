8b0daf5f9dacbb1c2fd033cbfeaed1416c8b8ac8
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-19 19:40:59 -0600

    Update Aahhh.java

--- Aahhh.java
+++ Aahhh.java
@@ -4,13 +4,18 @@ import java.io.InputStreamReader;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Aaah
+ https://open.kattis.com/problems/aaah
  */
 public class Aahhh {
     public static void main(String[] args) throws Exception {
+        //read input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String a = br.readLine();
         String b = br.readLine();
+        //if A is longer print no
         if(a.length() < b.length())System.out.println("no");
+        //else print go
         else System.out.println("go");
     }
 }
