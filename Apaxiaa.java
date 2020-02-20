ebf7fe2742c3cd2310d016b3ede09850eb16d435
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-19 20:02:01 -0600

    Update Apaxiaa.java

--- Apaxiaa.java
+++ Apaxiaa.java
@@ -1,18 +1,27 @@
 
 import java.util.Scanner;
+/*
+Kattis Solution to Apaxiaa
+https://open.kattis.com/problems/apaxiaaans
+*/
 
 public class Apaxiaa {
 
     public static void main(String[] args) {
+        //read input
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        //loop through every character and replace all consecutive characters with one character
         for (int i = 0; i < input.length() - 1; i++) {
             if (input.charAt(i) == input.charAt(i + 1)) {
                 int a = i + 1;
+                //find last consecutive same character
                 while (a < input.length() && input.charAt(a) == input.charAt(i)) a++;
+                //remove all of that character except for one
                 input = input.substring(0,i) + input.substring(a-1);
             }
         }
+        //print output
         System.out.println(input);
     }
 }
