54a5b3a70091deee74437c9678c48c2b2d3ea0ea
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-23 19:10:37 -0600

    Update BaconEggs.java

--- BaconEggs.java
+++ BaconEggs.java
@@ -14,18 +14,25 @@ public class BaconEggs {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //read in customers orders
         Scanner sc = new Scanner(System.in);
         int customers = Integer.parseInt(sc.nextLine());
+        //while we still have more day's orders to read
         while (customers != 0) {
+            //map each food to a list of customers that ordered it
             Map<String, ArrayList<String>> customerList = new HashMap<String, ArrayList<String>>();
+            //for each customer in that day's order
             for (int i = 0; i < customers; i++) {
+                //read scanner off of customer's order
                 Scanner line = new Scanner(sc.nextLine());
                 String customer = line.next();
+                //loop through each food item that customer ordered
                 while (line.hasNext()) {
                     String food = line.next();
+                    //if the food has already been encountered, simply add customer to that list
                     if ((customerList.containsKey(food))) {
                         customerList.get(food).add(customer);
-
+                    //food hasn't been encountered before, so add it to the map
                     } else {
                         customerList.put(food, new ArrayList<String>());
                         customerList.get(food).add(customer);
@@ -33,12 +40,16 @@ public class BaconEggs {
                 }
 
             }
+            //list to hold each food
             ArrayList<String> names = new ArrayList<String>();
+            //add all foods to the names list and sort the list of customer's that ordered that food
             for (String key : customerList.keySet()) {
                 names.add(key);
                 Collections.sort(customerList.get(key));
             }
+            //sort the list of food's alphabetically
             Collections.sort(names);
+            //print output
             for (String name : names) {
                 String output = "";
                 output += name + " ";
@@ -48,6 +59,7 @@ public class BaconEggs {
                 System.out.println(output.trim());
             }
             System.out.println();
+            //move onto next day
             customers = Integer.parseInt(sc.nextLine());
         }
     }
