a7929b5eb849bc787c02c0e5e5815001c9e6451d
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-02-29 20:11:52 -0600

    Update Cantina.java

--- Cantina.java
+++ Cantina.java
@@ -1,98 +1,86 @@
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-
 /**
  * @author L-Sternhagen
  Solution to kattis problem Cantina of Babel
  https://open.kattis.com/problems/cantinaofbabel
  */
-public class Cantina {
-    public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        Person[] people = new Person[num];
-        for(int a = 0; a < num; a++){
-            String[] line = br.readLine().split(" ");
-            String name = line[0];
-            Set<String> under = new HashSet<String>();
-            
-            for(int s = 2; s < line.length; s++)under.add(line[s]);
-            people[a] = new Person(name, line[1],under);
-        }
-        Map<String, Integer> howMany = new HashMap<String, Integer>();
-        for(Person i : people)howMany.put(i.name, 1);
-        for(int i = 0; i < num; i++){
-            for(int k = 0; k < num; k++){
-                if(i==k)continue;
-                if(bfs(people[i],people[k],people) && bfs(people[k],people[i],people)){
-                    howMany.put(people[i].name,howMany.get(people[i].name)+1);
+import java.util.*;
+public class CantinaOfBabel {
+    public static void main(String[] args) {
+        //read input
+        Scanner sc = new Scanner(System.in);
+        int numOfPeople = Integer.parseInt(sc.nextLine());
+        Person[] people = new Person[numOfPeople];
+        for (int i = 0; i < numOfPeople; i++)people[i] = new Person(sc.nextLine().split(" "));
+        //int[] holds which group each person is in
+        int[] groups = new int[numOfPeople];
+        //initialize each person to their own group
+        for (int i = 0; i < numOfPeople; i++)groups[i] = i;
+        //loop through each pair of people and bfs to eachother
+        for (int i = 0; i < numOfPeople; i++){
+            for (int j =0; j < numOfPeople; j++){
+                if(i==j)continue;
+                //if we can bfs each way, the two people can converse
+                if(bfs(i, j, people) && bfs(j,i,people)){
+                    //find which group i is in.
+                    int groupa = groups[i];
+                    //initialize everyone in the group of i to now be in the group of j
+                    for(int t = 0; t< numOfPeople; t++){
+                        if(groups[t]==groupa)groups[t] = groups[j];
+                    }
                 }
             }
         }
-        ArrayList<Integer> n = new ArrayList<Integer>();
-        for(String i: howMany.keySet()){
-            n.add(howMany.get(i));
+        //find the count of how many people are in each group
+        int[] countGroups = new int[numOfPeople];
+        for(int i = 0 ; i < groups.length; i++)countGroups[groups[i]]++;
+        //find the maximum group size
+        int max = -1;
+        for(int i = 0; i < countGroups.length; i++){
+            if(max < countGroups[i])max = countGroups[i];
         }
-     
-        System.out.println(n.size()-findMax(n));
+        //print out how many people are not in the largest group
+        System.out.println(numOfPeople-max);
+        
+        
     }
-    static int findMax(ArrayList<Integer> a){
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < a.size(); i++){
-            if(max < a.get(i))max = a.get(i);
-        }
-        return max;
-    }
-    static int findMin(ArrayList<Integer> a){
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i < a.size(); i++){
-            if(min > a.get(i))min = a.get(i);
-        }
-        return min;
-    }
-    static boolean bfs(Person start, Person end, Person[] all){
-        for(Person g: all)g.visited = false;
-        Queue<Person> q = new LinkedList<Person>();
-        q.add(start); start.visited = true;
+    //breadth first search from one person to another person in array P
+    public static boolean bfs(int start, int end, Person[] p){
+        //array to hold people we have already been to
+        int[] marked = new int[p.length];
+        //queue for bfs
+        Queue<Integer> q = new LinkedList<Integer>();
+        //add and mark start to the queue
+        q.add(start);
+        marked[start] = 1;
+        //loop while we have more people to visit
         while(!q.isEmpty()){
-           Person cur = q.poll();
-            if(cur.equals(end))return true;
-            for(Person a: all){
-                if(cur.canTalk(a) && !a.visited){
-                    q.add(a);
-                    a.visited = true;
+            int cur = q.poll();
+            //if we reached end person quit
+            if(cur == end)return true;
+            for (int i = 0; i < p.length; i++){
+                if(i==cur)continue;
+                Person current_person = p[i];
+                //if we haven't visited the person and they can converse then add new person to queue
+                if(marked[i]==0 && current_person.understands.contains(p[cur].speak)){
+                    marked[i] = 1;
+                    q.add((i));
                 }
             }
         }
-        return false;
+        //two people cannot converse
+        return false;        
     }
-    public static class Person{
-        String name;
+    //class to hold which person, the language they speak, and all other languages they understand
+    public static class Person {
         String speak;
-        Set<String> understand;
-        boolean visited;
-        public Person(String name, String speak, Set<String> understand){
-            this.name = name;
-            this.speak = speak;
-            this.understand = understand;
-            visited = false;
-        }
-        public boolean canTalk(Person a){
-            if(a.speak.equals(speak))return true;
-            return a.understand.contains(speak);
-        }
-        @Override
-        public boolean equals(Object o){
-            Person a = (Person)o;
-            return this.name.equals(a.name);
+        Set<String> understands;
+        //initialize person
+        public Person(String[] u){
+            this.speak = u[1];
+            Set<String> a = new HashSet<String>();
+            for (int i = 1; i < u.length; i++)a.add(u[i]);
+            this.understands = a;
         }
     }
 }
+
