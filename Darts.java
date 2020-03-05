fc3c7730521d7f67f325677935b93e93c1c2963a
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-04 19:40:37 -0600

    Update Darts.java

--- Darts.java
+++ Darts.java
@@ -1,23 +1,27 @@
 /**
  * @author L-Sternhagen
+ Solution to kattis problem Darts
+ https://open.kattis.com/problems/dartscores
  */
 
 import java.util.*;
 public class Darts {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        //read number of testcases and loop through them
         int testCases = sc.nextInt(); sc.nextLine();
         for(int zz = 0; zz < testCases; zz++){
-            
+            //find number of throws and set initial score to be 0
             int thro = sc.nextInt(); sc.nextLine();
             int score = 0;
             for(int qq = 0; qq < thro; qq++){
+                // read the x y coordinates of the throw
                 double x = sc.nextDouble();
                 double y = sc.nextDouble();
                 sc.nextLine();
-                
+                //get the length from the center
                 double length = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+                //increment score by which circle the dart landed in.
                 if(length <= 20){score+=10;}
                 else if(length <=40){score+=9;}
                 else if(length <=60){score+=8;}
@@ -30,12 +34,8 @@ public class Darts {
                 else if(length <=200){score+=1;}
                 
             }
-            System.out.println(score);
-        
-        
-            
+            //print output
+            System.out.println(score);  
         }
-        
-        
     }
 }
