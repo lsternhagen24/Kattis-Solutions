e4e288af747f0d7e342cd558c546f2db53aff468
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-08 13:19:30 -0500

    Update Filip.java

--- Filip.java
+++ Filip.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Filip
+ https://open.kattis.com/problems/filip
  */
 public class Filip {
     public static void main(String[] args) {
+        //read input
         Scanner sc = new Scanner(System.in);
         String[] line = sc.nextLine().split(" ");
+        //reverse the strings and print the maximum of the two reversed values
         System.out.println(Math.max(Integer.parseInt(reverseString(line[0])), Integer.parseInt(reverseString(line[1]))));
         
     }
+    //reverse a string input
     public static String reverseString(String a){
         String out = "";
         for(int i = a.length()-1; i >=0; i--)out+=a.charAt(i) + "";
