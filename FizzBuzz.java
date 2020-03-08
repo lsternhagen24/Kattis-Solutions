aa066eedf068a6adf2d973095c8a96c9de993395
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-08 13:29:48 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,34 +1,29 @@
-
-
 import java.util.Scanner;
-
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem FizzBuzz
+ https://open.kattis.com/problems/fizzbuzz
  */
 public class FizzBuzz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //read input
         String[] in  = sc.nextLine().split(" ");
-        int[] in2 = new int[in.length];
-        for(int a = 0; a <in.length; a++)in2[a]=Integer.parseInt(in[a]);
-        int x = in2[0];
-        int y = in2[1];
-        int n = in2[2];
+        int x = Integer.ParseInt(in[0]);
+        int y = Integer.ParseInt(in[1]);
+        int n = Integer.ParseInt(in[2]);
         
+        //loop through and print output
         for(int i = 1; i <= n ;i++){
             if(isDivisible(i,x) && isDivisible(i,y))System.out.println("FizzBuzz");
             else if(isDivisible(i,x))System.out.println("Fizz");
             else if(isDivisible(i,y))System.out.println("Buzz");
             else System.out.println(i);
-        
-        
-        }
-                
+        }           
     }
+    //return true is a is divisible by b, else false
     public static boolean isDivisible(int a , int b){
         if(a%b==0)return true;
         return false;
     }
-    
-    
 }
