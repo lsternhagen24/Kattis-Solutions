7fdd11fa3b3194068a1d59ea17c5d278baafba84
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-08 13:34:45 -0500

    Update FlexibleSpaces.java

--- FlexibleSpaces.java
+++ FlexibleSpaces.java
@@ -8,17 +8,22 @@ import java.util.Set;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Flexible Places
+ https://open.kattis.com/problems/flexible
  */
 public class FlexibleSpaces {
     public static void main(String[] args) throws Exception{
+        //read input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] data = br.readLine().split(" ");
         int width = Integer.parseInt(data[0]);
         int n = Integer.parseInt(data[1]);
         String[] input = br.readLine().split(" ");
+        //initialize all numbers in input and parse into Integers
         int[] nums = new int[n+2]; int pos = 1; nums[0] = 0;
         for(String s: input)nums[pos++] = Integer.parseInt(s);
         nums[pos] = width;
+        //set to hold all possible widths
         Set<Integer> all = new HashSet<Integer>();
         for(int i = n+1; i > 0; i--){
             int cur = nums[i];
@@ -27,6 +32,7 @@ public class FlexibleSpaces {
                 all.add(cur-j);
             }
         }
+        //find and print output
         Integer[] out = all.toArray(new Integer[all.size()]);
         Arrays.sort(out);
         String output = "";
