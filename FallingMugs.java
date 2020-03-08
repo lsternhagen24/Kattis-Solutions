75af306b9b96e6442406393f44bf5e98f501703f
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-08 13:10:44 -0500

    Update FallingMugs.java

--- FallingMugs.java
+++ FallingMugs.java
@@ -2,26 +2,22 @@ import java.util.Scanner;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Falling Mugs
+ https://open.kattis.com/problems/falling
  */
 public class FallingMugs {
     public static void main(String[] args) {
+        //read input
         Scanner sc = new Scanner(System.in);
         long d = sc.nextLong();
         long cur = 0;
-       // long oldDiff = 1;
+        //loop until we reach the distance or we get too fast
         while(true){
             long newA = cur * cur;
             double newB = Math.sqrt(d+newA);
             if(newB%1==0){System.out.println(cur + " " + (int)newB);break;}
             else if(cur>10000000){System.out.println("impossible");break;}
             else {cur++; }
-        
         }
-        
-        
-        
-        
-        
-        
     }
 }
