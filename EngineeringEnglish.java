b29526c5b9863b5d279bec5260d9c67c46dd9727
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-08 12:22:14 -0500

    Update EngineeringEnglish.java

--- EngineeringEnglish.java
+++ EngineeringEnglish.java
@@ -6,22 +6,31 @@ import java.util.Set;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Engineering English
+ https://open.kattis.com/problems/engineeringenglish    
  */
 public class EngineeringEnglish {
     public static void main(String[] args)throws Exception {
+        //stream reader for input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
+        //set to contain words we have already seen
         Set<String> h = new HashSet<String>();
+        //loop through all input
         while((line = br.readLine())!= null){
             String out = "";
+            //split on all words
             String[] in = line.split(" ");
             for(String i: in){
+                //if we have already seen the word then add a period
                 if(h.contains(i.toLowerCase()))out+=". ";
                 else {
+                    //otherwise add the word to our set and add the word
                     h.add(i.toLowerCase());
                     out+= i + " ";
                 }
             }
+            //print output
             System.out.println(out);
         }
     }
