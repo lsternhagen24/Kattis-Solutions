4293b4eb7fa7074458526eebc3480d1331f4fc02
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-08 13:21:29 -0500

    Update FireFly.java

--- FireFly.java
+++ FireFly.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 /**
  *
+ Solution To Kattis Problem Fire Fly
+ https://open.kattis.com/problems/firefly
  */
 public class FireFly {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        //read input
         Scanner sc = new Scanner(System.in);
-        
         int n = sc.nextInt();
         int h = sc.nextInt();
-        
+        //read wall counts from ceiling and ground
         int[] cCount = new int[h];
         int[] gCount = new int[h];
         
@@ -34,20 +33,14 @@ public class FireFly {
             gg=gg+gCount[i];
             oCount[i]+=gg;
         }
-                            
+        //check for the minumum needed                            
         int minO = Integer.MAX_VALUE;
         int minCount = 0;
         for(int i = 0; i < h; i++){
             if(oCount[i] < minO){minO=oCount[i];minCount=1;}
             else if(minO==oCount[i])minCount++;
-            
         }
+        //print output
         System.out.println(minO + " " + minCount);
-        
-        
-        
-        
-        
     }
-    
 }
