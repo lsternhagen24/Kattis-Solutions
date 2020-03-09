a0a7754187dd155b5396c4d3b19ea4e3ed7e70ed
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-09 13:17:53 -0500

    Update FlyingSafely.java

--- FlyingSafely.java
+++ FlyingSafely.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis problem Flying Safely
  */
 public class FlyingSafely {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int test_cases = sc.nextInt(); sc.nextLine();
+        //loop through each test case
         for(int j = 0; j < test_cases; j++){
             int cities = sc.nextInt();
             int pilots = sc.nextInt();
             sc.nextLine();
             for(int a = 0; a < pilots; a++)sc.nextLine();
-            
+            //cities - 1 is always solution due to the fact that the graph is alwasy connected
             System.out.println(cities-1);
         }
     }
