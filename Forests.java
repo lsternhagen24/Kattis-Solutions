9a26d1a33a78a964a173406e1ebc205f7e56abea
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-09 13:33:57 -0500

    Update Forests.java

--- Forests.java
+++ Forests.java
@@ -2,12 +2,17 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+/*
+Solution to Kattis Problem Forests
+https://open.kattis.com/problems/forests
+*/
 
 public class Forests {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int p = sc.nextInt();
         int t = sc.nextInt(); sc.nextLine();
+        //hold each person's opinions
         int[][] people = new int[p][t];
         //read input
         while(sc.hasNextLine()){
@@ -18,7 +23,9 @@ public class Forests {
         }
         //add all arrays to set
         Set<String> opinions = new HashSet<String>();
+        //add each person's tree to a set to remove duplicates
         for(int i = 0; i < p; i++)opinions.add(Arrays.toString(people[i]));
+        //print output
         System.out.println(opinions.size());
     }
 }
