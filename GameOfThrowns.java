fb0eaeaa7f0ea851a91d1977d156934a158ee4cc
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-10 13:47:10 -0500

    Update GameOfThrowns.java

--- GameOfThrowns.java
+++ GameOfThrowns.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Game Of Throwns
+ https://open.kattis.com/problems/throwns
  */
 public class GameOfThrowns {
     public static void main(String[] args) {
@@ -12,6 +14,7 @@ public class GameOfThrowns {
         String[] input = sc.nextLine().split(" ");
         //read in commmands
         for(int i = 0; i < input.length; i++){
+            //if we get to an 'undo' command then remove that many commands before it
             if(input[i].equals("undo")){
                 i++;
                 int r = Integer.parseInt(input[i]);
@@ -26,6 +29,7 @@ public class GameOfThrowns {
             if(pos < 0)pos = n-(pos*-1);
             if(pos>=n)pos-=n;
         }
+        //print output
         System.out.println(pos);
     }
 }
