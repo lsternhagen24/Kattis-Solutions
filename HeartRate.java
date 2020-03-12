1c0b8e566736a962116065e2fbf6912f8b52c5aa
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-12 13:36:13 -0500

    Update HeartRate.java

--- HeartRate.java
+++ HeartRate.java
@@ -1,14 +1,18 @@
-
 import java.util.Scanner;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Heart Rate
+ https://open.kattis.com/problems/heartrate
  */
 public class HeartRate {
     public static void main(String[] args) {
+        //scanner for reading input
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt(); sc.nextLine();
+        //loop through all test cases
         for(int i = 0; i < t; i++){
+            //read b and a, then print output
             double b = sc.nextDouble();
             double a = sc.nextDouble();
             System.out.println((60/a)*(b-1) + " "  + (60/a) * b + " " + (60/a) * (b+1));
