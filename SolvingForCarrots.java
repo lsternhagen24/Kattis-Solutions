d7bc9d96f8e7c8d0804435fc8e21e86af3818565
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:36:14 -0500

    Update SolvingForCarrots.java

--- SolvingForCarrots.java
+++ SolvingForCarrots.java
@@ -4,11 +4,15 @@ import java.io.InputStreamReader;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Solving For Carrots
+ https://open.kattis.com/problems/carrots
  */
 public class SolvingForCarrots {
     public static void main(String[] args)throws Exception {
+        //reader for reading input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] a = br.readLine().split(" ");
+        //solution will always be second number.
         System.out.println(a[1]);
     }
 }
