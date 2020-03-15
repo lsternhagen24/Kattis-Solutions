4d60163dcdc161b253efbcee5b42adfecb7389d2
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:13:49 -0500

    Update Oddities.java

--- Oddities.java
+++ Oddities.java
@@ -1,16 +1,21 @@
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Oddities
+ https://open.kattis.com/problems/oddities
  */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class Oddities {
     public static void main(String[] args)throws Exception {
+        //reader for reading input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCases = Integer.parseInt(br.readLine());
+        //loop through each test case
         for(int a = 0; a < testCases; a++){
-        int cur = Integer.parseInt(br.readLine());
-        if(cur%2==0)System.out.println(cur + " is even");
-        else System.out.println(cur + " is odd");
+            //calculate if current is odd and print output
+            int cur = Integer.parseInt(br.readLine());
+            if(cur%2==0)System.out.println(cur + " is even");
+            else System.out.println(cur + " is odd");
         }
     }
 }
