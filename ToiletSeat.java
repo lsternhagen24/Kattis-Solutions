158fe0eee19d0af7ee12bae7550c5224d4cac8a9
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:45:17 -0500

    Update ToiletSeat.java

--- ToiletSeat.java
+++ ToiletSeat.java
@@ -6,12 +6,10 @@ import java.util.Scanner;
 /**
  *
  * @author lucas
+ Solution to Kattis Problem Toilet Seat
+ https://open.kattis.com/problems/toilet
  */
 public class ToiletSeat {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String info = sc.nextLine();
@@ -19,8 +17,10 @@ public class ToiletSeat {
         int down = 0;
         char OriginalPosition = info.charAt(0);
         char firstPerson = info.charAt(1);
+        //arraylist of characters to hold position of people
         ArrayList<Character> people = new ArrayList<Character>();
         people.add(OriginalPosition);
+        //calculate the number of flips up and flips down needed
         for (int i = 1; i < info.length(); i++) {
             if (info.charAt(i) == 'U') {
                 up++;
@@ -30,6 +30,7 @@ public class ToiletSeat {
             }
             people.add(info.charAt(i));
         }
+        //calculate and print out number of flips
         if (OriginalPosition == 'U') {
             System.out.println(down * 2);
             if (firstPerson == 'U') {
@@ -38,14 +39,17 @@ public class ToiletSeat {
                 System.out.println((up * 2) + 1);
             }
             int changes = 0;
+            //calculate changes
             for (int j = 1; j < people.size(); j++) {
                 if (people.get(j) != people.get(j - 1)) {
                     changes++;
                 }
             }
+            //print number of changes
             System.out.println(changes);
 
         }
+        //same logic as above for original position of down
         if (OriginalPosition == 'D') {
             if (firstPerson == 'U') {
                 System.out.println(down * 2 + 1);
@@ -66,5 +70,4 @@ public class ToiletSeat {
             System.out.println(changes);
         }
     }
-
 }
