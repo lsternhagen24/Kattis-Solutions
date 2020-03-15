bdcdb392983c1c66a1bb2bc5dbba3ae308102b86
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:12:20 -0500

    Update Natjecanje.java

--- Natjecanje.java
+++ Natjecanje.java
@@ -6,22 +6,29 @@ import java.util.Set;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Natjecanje
+ https://open.kattis.com/problems/natjecanje
  */
 public class Natjecanje {
     public static void main(String[] args)throws Exception {
+        //reader for reading input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
+        //set for containing all the broken kayaks
         Set<Integer> broken = new HashSet<Integer>(); 
         in = br.readLine().split(" ");
         for(int i = 0; i < in.length; i++)broken.add(Integer.parseInt(in[i]));
+        //set for containing all the reserved kayaks
         Set<Integer> reserve = new HashSet<Integer>();
         in = br.readLine().split(" ");
         for(int i = 0; i < in.length; i++)reserve.add(Integer.parseInt(in[i]));
+        //loop through all the reserved kayaks and remove them from the broken set.
         for(Integer i: reserve){
             if(broken.contains(i))broken.remove(i);
             else if(broken.contains(i-1))broken.remove(i-1);
             else if(broken.contains(i+1))broken.remove(i+1);
         }
+        //print output
         System.out.println(broken.size());
     }
 }
