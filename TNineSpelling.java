482a68ae71b99c863fe10c3731168f0cf38e3c00
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:42:36 -0500

    Update TNineSpelling.java

--- TNineSpelling.java
+++ TNineSpelling.java
@@ -1,13 +1,16 @@
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem T9 Spelling
+ https://open.kattis.com/problems/t9spelling
  */
 import java.util.*;
 public class TNineSpelling {
     public static void main(String[] args) {
+        //Scanner for reading input
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine().trim());
         for(int i = 0; i <n; i++){
-            
+            //map each character to the number it coorelates to
             Map<Character, Integer> g = new HashMap<Character, Integer>();
             g.put('a', 2);
             g.put('b',2);
@@ -36,6 +39,7 @@ public class TNineSpelling {
             g.put('y', 9);
             g.put('z',9);
             g.put(' ', 0);
+            //map each number to the string it can reach
             Map<Integer,String> h = new HashMap<Integer,String>();
             h.put(2, "abc");
             h.put(3, "def");
@@ -47,22 +51,22 @@ public class TNineSpelling {
             h.put(9, "wxyz");
             h.put(0, " ");
             
-            
+            //reach the message
             String message = sc.nextLine();
             int previous = -1;
             String output = "";
             for(int a = 0; a < message.length(); a++){
                 char c = message.charAt(a);
                 int j = g.get(c);
+                //if we are on the same number insert a pause
                 if(j==previous)output+=" ";
                 previous = j;
                 int k = h.get(j).indexOf(c+"")+1;
+                //add number to string
                 for(int l = 0; l < k; l++)output+=j+"";
-                
             }
-            System.out.println("Case #"+(1+i)+": "+output);
-        
+            //print output
+            System.out.println("Case #"+(1+i)+": "+output);\
         }
-        
     }
 }
