ac632649db6f5feb51a31432195259edadf12acc
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:34:53 -0500

    Update Simon.java

--- Simon.java
+++ Simon.java
@@ -6,16 +6,21 @@ import java.util.regex.Pattern;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Simon
+ https://open.kattis.com/problems/simon
  */
 public class Simon {
     public static void main(String[] args)throws Exception {
+        //reader for reading input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCases = Integer.parseInt(br.readLine());
-        for(int i = 0; i < testCases; i++){;
-        String input = br.readLine();
-        Matcher m = Pattern.compile("^simon says (.*)").matcher(input);
-        if(m.find())System.out.println(m.group(1));
-        else System.out.println("");
+        //loop through all test cases
+        for(int i = 0; i < testCases; i++){
+            String input = br.readLine();
+            //use regex to find the number matches and all strings that match, then print
+            Matcher m = Pattern.compile("^simon says (.*)").matcher(input);
+            if(m.find())System.out.println(m.group(1));
+            else System.out.println("");
         }
     }
 }
