57dcddc09e18002217cd7646615763a06685ff72
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 18:01:00 -0500

    Update VirtualFriends.java

--- VirtualFriends.java
+++ VirtualFriends.java
@@ -5,28 +5,37 @@ import java.util.*;
 
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Virtual Friends
+ https://open.kattis.com/problems/virtualfriends
  */
 public class VirtualFriends {
     static ArrayList<Integer> size;
     static ArrayList<Integer> position;
     static Map<String,Integer> network;
     public static void main(String[] args)throws Exception {
+        //reader for reading input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCases = Integer.parseInt(br.readLine());
+        //loop through all test cases
         for(int q = 0; q < testCases; q++){
             int iterator = 0;
+            //initilize variables
             size = new ArrayList<Integer>();position = new ArrayList<Integer>();network = new HashMap<String, Integer>();
             int relationships = Integer.parseInt(br.readLine());
+            //loop through all relationships
             for(int v = 0; v < relationships; v++){
         String[] people = br.readLine().split(" ");
         String person1 = people[0]; String person2 = people[1];
+        //add two people to the netword and union their roots
         if(!network.containsKey(person1)){network.put(person1, iterator); position.add(iterator++); size.add(1);}
         if(!network.containsKey(person2)){network.put(person2, iterator); position.add(iterator++); size.add(1);}
         union(person1,person2);
+            //print output
             System.out.println(size.get(find(person1)));
             }
             }
     }
+    //find's a person
     public static int find(String person){
         int a = network.get(person);
         while (a != position.get(a)) {
@@ -35,6 +44,7 @@ public class VirtualFriends {
         }
         return a;
     }
+    //union two root nodes
     public static void union(String a, String b) {
         int root1 = find(a);
         int root2 = find(b);
