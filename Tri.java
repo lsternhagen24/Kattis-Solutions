6e8b10a9d7bd40c2ecef8482d88b6e16ae024a45
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:55:12 -0500

    Update Tri.java

--- Tri.java
+++ Tri.java
@@ -1,16 +1,18 @@
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Tru
+ https://open.kattis.com/problems/tri
  */
 import java.util.*;
 public class Tri {
     
     public static void main(String[] args) {
+        //scanner for reading input
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        
-        
+        //calculate output and print
         if(a+b==c)System.out.println(a+"+"+b+"="+c);
         else if(c+b==a)System.out.println(a+"="+b+"+"+c);
         else if(a*b==c)System.out.println(a+"*"+b+"="+c);
@@ -19,7 +21,5 @@ public class Tri {
         else if(b-c==a)System.out.println(a+"="+b+"-"+c);
         else if(a/b==c)System.out.println(a+"/"+b+"="+c);
         else if(b/c==a)System.out.println(a+"="+b+"/"+c);
-
-
     }
 }
