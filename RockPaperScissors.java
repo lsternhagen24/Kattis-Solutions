dd782dc5224aea3bdce91f96e5fa2f85eed49c50
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:29:54 -0500

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -3,22 +3,27 @@ import java.io.InputStreamReader;
 import java.util.*;
 /**
  * @author L-Sternhagen
+ Solution to Kattis Problem Rock Paper Scissors
+ https://open.kattis.com/problems/rockpaperscissors
  */
 public class RockPaperScissors {
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int u=0;
+        //loop until we reach the end of input
         while(true){
             String[] line = br.readLine().split(" ");
             int players = Integer.parseInt(line[0]);
             if(players==0)break;
             if(u!=0)System.out.println();
+            //array for holding each player
             Player[] allP = new Player[players];
             for(int g = 0; g <players; g++)allP[g]=new Player();
+            //read the number of games
             int games = Integer.parseInt(line[1]);
-            
+            //get the number of lines
             int lines = (players * games * (players-1))/2;
-
+            //loop through all lines and get the results
             for(int a = 0; a<lines; a++){
                 String[] game = br.readLine().split(" ");
                 if(game[1].equals("rock")&&game[3].equals("scissors")){allP[Integer.parseInt(game[0])-1].wins++;allP[Integer.parseInt(game[2])-1].loses++; }
@@ -28,6 +33,7 @@ public class RockPaperScissors {
                 if(game[1].equals("scissors")&&game[3].equals("rock")){allP[Integer.parseInt(game[0])-1].loses++;allP[Integer.parseInt(game[2])-1].wins++; }
                 if(game[1].equals("paper")&&game[3].equals("scissors")){allP[Integer.parseInt(game[0])-1].loses++;allP[Integer.parseInt(game[2])-1].wins++; }
             }
+            //calculate each players percentage of winning and print output
             for(int v = 0; v<players; v++){
                 double d = allP[v].wins+allP[v].loses;
                 if(d==0)System.out.println("-");
@@ -39,8 +45,8 @@ public class RockPaperScissors {
             }
             u++;
         }
-        
     }
+    //custom round function
     public static String round(double a){
         double roundOff = Math.round(a * 1000.0) / 1000.0;
         String output = roundOff+"";
@@ -48,7 +54,7 @@ public class RockPaperScissors {
         if(rr<4){for(int r =rr; r<4; r++)output+="0";}
         return output;
     }
-    
+    //each player needs a number of games won and lose, initialize to 0
     static class Player{
         int wins;
         int loses;
@@ -56,6 +62,5 @@ public class RockPaperScissors {
             wins = 0;
             loses = 0;
         }
-    
     }
 }
