a0bc1a0458ef8e64e2dd6569a434397dec443630
lsternhagen24 <59424338+lsternhagen24@users.noreply.github.com>
2020-03-15 17:22:36 -0500

    Update PhoneList.java

--- PhoneList.java
+++ PhoneList.java
@@ -5,49 +5,56 @@ import java.util.*;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author lucas
+ Solution to Kattis Problem Phone List
+ https://open.kattis.com/problems/phonelist
  */
 public class PhoneList {
 
     public static void main(String[] args) throws Exception {
+        //reader for reading input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCases = Integer.parseInt(br.readLine());
+        //loop through each testcase
         for (int i = 0; i < testCases; i++) {
             String output = "YES";
             int min = 11;
             int next = Integer.parseInt(br.readLine());
+            //set to contain all phone numbers we have previously seen
             Set<String> mySet = new HashSet<String>();
+            //array for holding all current strings
             myString[] al = new myString[next];
             for (int a = 0; a < next; a++) {
                 String line = br.readLine();
+                //find the minimum line length
                 if (min > line.length()) {
                     min = line.length();
                 }
+                //read in new phone number
                 al[a] = new myString(line);
             }
+            //sort phone numbers
             Arrays.sort(al, Collections.reverseOrder());
+            //loop through every phone number
             for (int v = 0; v < al.length; v++) {
                 String current = al[v].input;
+                //if we already have seen the phone number return NO 
                 if (mySet.contains(current)) {
                     output = "NO";
                 }
+                //add the phone number and all possibilites that are at least as long as the minimum length
                 mySet.add(current);
                 for (int a = current.length(); a >= min; a--) {
                     mySet.add(current.substring(0, a));
                 }
             }
+            //print output
             System.out.println(output);
         }
     }
-
+    //custom string for sorting based on length instead of usual string comparison
     public static class myString implements Comparable<myString>, Comparator<myString> {
 
         String input;
